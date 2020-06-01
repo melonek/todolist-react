8e46c31ebb53d7448c9dbbf1d2ef71dafeeeef8b
melon <meloniusz@hotmail.com>
2020-06-01 22:42:46 +0800

    get from axios works

--- src/App.js
+++ src/App.js
@@ -5,29 +5,20 @@ import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
 import About from "./components/pages/About";
 import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
 import "./App.css";
 
 class App extends Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Take out the trash",
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: "Dinner with wife",
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: "Meeting with boss",
-        completed: false,
-      },
-    ],
+    todos: [],
   };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => this.setState({ todos: res.data }));
+  }
 
   //Toggle Complete
   markComplete = (id) => {
